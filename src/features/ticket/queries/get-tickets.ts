99e43c499c8fb3ce8845a15d5e68a45fea67a26d
dzamicm <miodrag.dzamic@tipico.com>
2025-07-23 16:13:18 +0200

    Add Loading, Error, Error Boundary and notFound

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -4,9 +4,12 @@ import { Ticket } from "../types";
 export const getTickets = async (): Promise<Ticket[]> => {
   await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating a delay
 
+  // throw new Error("Failed to fetch tickets"); // Simulating an error
+
   return new Promise((resolve) => {
     resolve(initialTickets);
   });
+
   // Same as the commented code below, but using async/await syntax
   //   return new Promise((resolve) => {
   //     setTimeout(() => {

ce8b3a2b2fc1bc8f204c5f4a0c7ddfac11dc6296
dzamicm <miodrag.dzamic@tipico.com>
2025-07-28 18:36:14 +0200

    Server Queries - Add Prisma client, DB queries and ORM types

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,12 +1,22 @@
-import { initialTickets } from "@/data";
-import { Ticket } from "../types";
+// import { initialTickets } from "@/data";
+// import { Ticket } from "../types";
 
-export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating a delay
+// export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
+//   await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating a delay
 
-  const maybeTicket = initialTickets.find((ticket) => ticket.id === ticketId);
+//   const maybeTicket = initialTickets.find((ticket) => ticket.id === ticketId);
 
-  return new Promise((resolve) => {
-    resolve(maybeTicket || null);
+//   return new Promise((resolve) => {
+//     resolve(maybeTicket || null);
+//   });
+// };
+
+import { prisma } from "@/lib/prisma";
+
+export const getTicket = async (id: string) => {
+  return await prisma.ticket.findUnique({
+    where: {
+      id,
+    },
   });
 };
--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -1,19 +1,29 @@
-import { initialTickets } from "@/data";
-import { Ticket } from "../types";
+import { prisma } from "@/lib/prisma";
 
-export const getTickets = async (): Promise<Ticket[]> => {
-  await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating a delay
-
-  // throw new Error("Failed to fetch tickets"); // Simulating an error
-
-  return new Promise((resolve) => {
-    resolve(initialTickets);
+export const getTickets = async () => {
+  return await prisma.ticket.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
   });
-
-  // Same as the commented code below, but using async/await syntax
-  //   return new Promise((resolve) => {
-  //     setTimeout(() => {
-  //       resolve(initialTickets);
-  //     }, 1000);
-  //   });
 };
+
+// import { initialTickets } from "@/data";
+// import { Ticket } from "../types";
+
+// export const getTickets = async (): Promise<Ticket[]> => {
+//   await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating a delay
+
+//   // throw new Error("Failed to fetch tickets"); // Simulating an error
+
+//   return new Promise((resolve) => {
+//     resolve(initialTickets);
+//   });
+
+//   // Same as the commented code below, but using async/await syntax
+//   //   return new Promise((resolve) => {
+//   //     setTimeout(() => {
+//   //       resolve(initialTickets);
+//   //     }, 1000);
+//   //   });
+// };

fe97e612a515b05eab3e7713209eb5a338e12c93
miodragdz <miodrag.dzamic@gmail.com>
2026-07-15 16:28:08 +0200

    Move RedirectToast to layout

--- src/components/redirect-toast.tsx
+++ src/components/redirect-toast.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import { toast } from "sonner";
 import { consumeCookiedByKey } from "@/actions/cookies";
@@ -7,6 +8,10 @@ import { consumeCookiedByKey } from "@/actions/cookies";
 // we do not use here hook useRedirectToast because all components which would use that hook would have to be client components
 // this way only this component is a client component
 const RedirectToast = () => {
+  // added because component RedirectToast is moved to layout and layout renders only once, so
+  // pathname added to useEffect to make sure it runs every time path changes and check for cookie
+  const pathname = usePathname();
+
   useEffect(() => {
     const showCookieToast = async () => {
       // Due to React Strict mode we have this run twice and double toast message
@@ -20,7 +25,7 @@ const RedirectToast = () => {
     };
 
     showCookieToast();
-  }, []);
+  }, [pathname]);
 
   return null;
 };

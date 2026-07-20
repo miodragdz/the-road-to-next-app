de43d6931ad01cd761b27efa6edb8f7dc35e9886
miodragdz <miodrag.dzamic@gmail.com>
2026-07-20 16:07:59 +0200

    Add ConfirmDialog

---
+++ src/components/confirm-dialog.tsx
@@ -0,0 +1,50 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
+import { Button } from "./ui/button";
+
+type ConfirmDialogProps = {
+  title?: string;
+  description?: string;
+  action: (formData: FormData) => Promise<void>;
+  trigger: React.ReactElement;
+};
+
+const ConfirmDialog = ({
+  title = "Are you absolutely sure?",
+  description = "This action cannot be undone. Make sure you understand the consequences.",
+  action,
+  trigger,
+}: ConfirmDialogProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel className="cursor-pointer">
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction className="cursor-pointer" asChild>
+            <form action={action}>
+              <Button type="submit">Confirm</Button>
+            </form>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export { ConfirmDialog };

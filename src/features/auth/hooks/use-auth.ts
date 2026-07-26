1fc35e901d473a7133ce9dd8de90c0dd84b01c8e
miodragdz <miodrag.dzamic@gmail.com>
2026-07-26 16:07:38 +0200

    Migrate Lucia to Oslo

--- src/features/auth/hooks/use-auth.ts
+++ src/features/auth/hooks/use-auth.ts
@@ -1,10 +1,10 @@
-import { User as AuthUser } from "lucia";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { getAuth } from "../queries/get-auth";
+import { User } from ".prisma/generated/client";
 
 const useAuth = () => {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  const [user, setUser] = useState<User | null>(null);
   const [isFetched, setFetched] = useState(false);
 
   const pathname = usePathname();

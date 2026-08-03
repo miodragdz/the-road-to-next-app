191bb83d2d096e5d3c9573d24ef47730acf6724a
miodragdz <miodrag.dzamic@gmail.com>
2026-08-03 15:04:10 +0200

    Add search for tickets by title

--- src/app/(authenticated)/tickets/page.tsx
+++ src/app/(authenticated)/tickets/page.tsx
@@ -7,8 +7,13 @@ import { Spinner } from "@/components/spinner";
 import { getAuth } from "@/features/auth/queries/get-auth";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
+import { SearchParams } from "@/features/ticket/search-params";
 
-const TicketsPage = async () => {
+type TicketsPageProps = {
+  searchParams: Promise<SearchParams>;
+};
+
+const TicketsPage = async ({ searchParams }: TicketsPageProps) => {
   const { user } = await getAuth();
 
   return (
@@ -24,7 +29,7 @@ const TicketsPage = async () => {
 
       <ErrorBoundary fallback={<Placeholder label="Something went wrong" />}>
         <Suspense fallback={<Spinner />}>
-          <TicketList userId={user?.id} />
+          <TicketList userId={user?.id} searchParams={await searchParams} />
         </Suspense>
       </ErrorBoundary>
     </div>

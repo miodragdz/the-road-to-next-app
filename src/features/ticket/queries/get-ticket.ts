dc52306c138f44c560fab4672b5bed4311897b4f
miodragdz <miodrag.dzamic@gmail.com>
2026-08-04 14:51:14 +0200

    Add public APIs - Route Handler

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,16 +1,3 @@
-// import { initialTickets } from "@/data";
-// import { Ticket } from "../types";
-
-// export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
-//   await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulating a delay
-
-//   const maybeTicket = initialTickets.find((ticket) => ticket.id === ticketId);
-
-//   return new Promise((resolve) => {
-//     resolve(maybeTicket || null);
-//   });
-// };
-
 import { prisma } from "@/lib/prisma";
 
 export const getTicket = async (id: string) => {
